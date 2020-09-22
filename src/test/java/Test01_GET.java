0625127408e7a0962f90740def415363d73cf3c6
bharatbarhate <bharat_automation@yahoo.com>
2020-09-21 17:49:47 -0700

    Restassured :: Test case #1 :: Header verificaion

---
+++ src/test/java/Test01_GET.java
@@ -0,0 +1,40 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+import  io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class Test01_GET {
+
+	@Test
+	void test_01()
+	{
+		Response response = get("https://reqres.in/api/users?page=2");
+		System.out.println(response.prettyPrint());
+		System.out.println("Response as String[prettyPeek] : "+response.prettyPeek().asString());
+		System.out.println("Response as Server : "+response.getHeader("Server"));
+		System.out.println("Response Body : "+response.getBody().asString());
+		System.out.println("Response as String : "+response.asString());
+		System.out.println("Response Status code : "+response.getStatusCode());
+		System.out.println("Response Status Line : "+response.getStatusLine());
+		System.out.println("Response Header : "+response.getHeader("content-type"));
+		System.out.println("Response get Time : "+response.getTime());
+
+
+		int status_code = response.getStatusCode();
+		Assert.assertEquals(status_code, 200);
+	}
+	@Test
+	void test_02()
+	{
+		given().
+			get("https://reqres.in/api/users?page=2").
+		then()
+			.statusCode(200)
+			.body("data.id[0]",equalTo(7));
+
+	}
+}
